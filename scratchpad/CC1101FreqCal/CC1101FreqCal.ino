232cc678bc97da9fe86333d5e100e2a0373c36e5
lnigra <lounigra@gmail.com>
2014-06-23 21:39:16 -0500

    Basic CC1101 frequency error measurement using GPS PPS as reference.

--- scratchpad/CC1101FreqCal/CC1101FreqCal.ino
+++ scratchpad/CC1101FreqCal/CC1101FreqCal.ino
@@ -1,26 +1,36 @@
 /*
-  CC1101 Crystal frequency error measurement.
+  Timer Interrupt using CC1101 GDO0 as external clock signal.
   
   Lou Nigra
   Far Horizons Lab
+  June 2014
   
+  Uses CC1101 GDO0 as external clock, by connecting externally
+  Pin 2 (connected to GDO0) to Pin 5, which is configured as T1,
+  the Timer 1 input clock. It is configured to interrupt on timer
+  comparison.
+  
+  External GPS PPS output is connected to Pin 3 and configured as
+  an external interrupt. The GPS should be configured to have PPS
+  output only when it has a fix.  
 */
 
 #include <SPI.h>
 #include <RADIOFH.h>
 
-// Local non-default config header exported from TI SmartRF Studio
-#include "cc1101_70cm_gfsk_2kb_5khz_manchester_P10dbm.h"
+#define CLKDIV 16.0
+#define GDOCFG 0x38
 
-const float xtalNomFreq = 26e6; // 70cm CC1101EM
-const int ppsPin = 3;
-
-volatile long count, oldCount, newCount;
-int clkDiv;
+const float xtalFreq = 26e6; // 70cm CC1101EM
+int measSeconds;
+long totalCount;
+float error;
+volatile long ppsCount, clkCount, timerVal;
 volatile boolean active;
 
 void setup() {
-  
+
+  measSeconds = 10;
   Serial.begin (9600); 
   SPI.begin ();
   SPI.setClockDivider(SPI_CLOCK_DIV4);   
@@ -28,51 +38,100 @@ void setup() {
   SPI.setDataMode(SPI_MODE0) ;
   CC1101.GDO_Set();
   CC1101.Reset(); // Default register settings
-  RegConfig(); // Non-default settings defined in config header.
+  
+  // Can't find max ext clock frequency for ATMega328, but
+  // empirically, CLK_XOSC/12 (0x37) seems like the max.
+  CC1101.WriteSingleReg( RADIO_IOCFG0, GDOCFG );
+ 
+  // SPI active interferes with interrupt somehow
+  SPI.end();
+  
+  // Disable all interrupts while messing with them.
   noInterrupts();
 
-  // PPS from tracking GPS or other source 
-  pinMode( ppsPin, INPUT );
+  // initialize Timer1 
+  TCCR1A = 0;
+  TCCR1B = 0;
+  TCNT1  = 0;
+
+  // Load Compare Match Register with counter value comparison. 
+  // This is the number of counts minus 1 between interrupts. 
+  OCR1A = 16000;            // compare match register
   
-  // XTAL freq / 4 => GDO0 output
-  CC1101.WriteSingleReg( RADIO_IOCFG0, 0x34 );
-  count = 0;
-  clkDiv = 4;
-    
-  attachInterrupt( 0, xtalDiv, RISING );
+  // Set timer to Clear Timer on Compare mode. Timer set to zero
+  // upon reaching compare value and continues counting.
+  TCCR1B |= (1 << WGM12);
+  
+  // Setting CS0-2 in Timer 1 configuration register to 111
+  // to enable ext clock on T1 input (pin 5) using rising edge.
+  TCCR1B |= (1 << CS12);
+  TCCR1B |= (1 << CS11);
+  TCCR1B |= (1 << CS10); 
+  
+  // Enable Timer Compare Interrupt.
+  // TIMSK1 |= (1 << OCIE1A);
+  
   attachInterrupt( 1, pps, RISING );
+  
+  ppsCount = 0L;
+  clkCount = 0L;
   active = false;
-  newCount = 0;
-  oldCount = 0;
 }
 
 void loop() {
   
+  // Run this twice and ignore the first. The first measurement 
+  // is always bogus. Have no idea why.
   interrupts();
-  while( !active );
-  while( active );
-  noInterrupts();
-  Serial.print( oldCount );Serial.println( newCount );
-  Serial.println( (float)( 
-      newCount - oldCount ) * clkDiv / xtalNomFreq - 1.0 );
+  while( ppsCount < measSeconds );
+  // mask timer1 interrupt
+  TIMSK1 &= ~(1 << OCIE1A);
+  noInterrupts(); // disables pps interrupt as well.
+  
+  ppsCount = 0L;
+  clkCount = 0L;
   active = false;
-  newCount = 0;
-  oldCount = 0;
+  
+  interrupts();
+  while( ppsCount < measSeconds );
+  noInterrupts();
+  TIMSK1 &= ~(1 << OCIE1A);  
+  totalCount = CLKDIV * ( ( ( (long)OCR1A + 1 ) * 
+                            ( clkCount - 1 ) ) + timerVal );
+  error = 1.0e+6 * ( 1.0 - (float)totalCount / 
+                    (float)ppsCount / xtalFreq );
+  float correction =( -error * 1.0e-6 * 16384.0 );
+  
+  interrupts();
+  Serial.print( "CC1101 XTAL Offset:" );
+  Serial.print( error, 1 );Serial.println( " ppm");
+  Serial.println( correction );
+  while( true );
+}
+
+ISR(TIMER1_COMPA_vect) { // Uses the ISR() macro
+
+  clkCount++;
+
+}
+
+void pps() {  // INT1_vect
+
+  if ( !active ) {
+    active = true;
+  //   reset timer1 to zero
+    TCNT1 = 0L;
+    clkCount = 0L;
+  // unmask timer1 compare interrupt
+    TIMSK1 |= (1 << OCIE1A);
+  } else {
+  //   read timer1 value
+    timerVal = TCNT1;
+  //   increment pps count 
+    ppsCount++;
+  }
   
 }
-
-void xtalDiv() {
-  count++;
-}
-
-void pps() {
-  oldCount = newCount;
-  newCount = count;
-  active = !active;
-}
-
-
-
 
 
 
